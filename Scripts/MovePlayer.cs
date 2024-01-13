a73517838abf4249a6bb3984fe386efb86ad8606
Jordan <jordan>
2024-01-13 12:14:45 -0500

    adds Player placeholder

---
+++ Scripts/MovePlayer.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    private bool playerLeftInput;
+    private bool playerRightInput;
+    private bool userJumped;
+    // private const float scaleMovement = 0.5f;
+    // private const float horizontalMovement = 0.5f;
+
+    private Rigidbody rigidBody;
+    // private Transform transform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        // transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerLeftInput = Input.GetKey("left");
+        playerRightInput = Input.GetKey("right");
+        userJumped = Input.GetButton("Jump");
+        
+    }
+
+    void FixedUpdate() {
+        if (playerLeftInput) {
+            rigidBody.AddForce(Vector3.left, ForceMode.VelocityChange);
+            userLeftInput = false;
+        }
+
+        if (playerRightInput) {
+            rigidBody.AddForce(Vector3.right, ForceMode.VelocityChange);
+            userRightInput = false;
+        }
+        if (userJumped) {
+            rigidBody.AddForce(Vector3.up, ForceMode.VelocityChange);
+            userJumped = false;
+        }
+    }
+}
