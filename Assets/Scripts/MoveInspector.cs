ef07acb597faef4e9da1d95cdcda66030f834cc9
fonuxxine <samira.dang@mail.utoronto.ca>
2024-01-13 14:03:59 -0500

    Inspector and health

---
+++ Assets/Scripts/MoveInspector.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveInspector : MonoBehaviour
+{
+    gameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
